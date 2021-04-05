71d45bcba1a82fe031b3ebe204f761f94d3fd4ce
dabbott12 <guycomputer@live.ca>
2021-04-04 17:33:49 -0700

    added gamemanager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,102 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int currentTurn;
+    public bool placingBuilding;
+    public BuildingType currentSelectedBuilding;
+
+    [Header("Current Resources")]
+    public int currentFood;
+    public int currentMetal;
+    public int currentOxygen;
+    public int currentEnergy;
+
+    [Header("Round Resource Increase")]
+    public int foodPerTurn;
+    public int metalPerTurn;
+    public int oxygenPerTurn;
+    public int energyPerTurn;
+
+    public static GameManager instance;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+            
+    }
+
+    public void EndTurn()
+    {
+        currentFood += foodPerTurn;
+        currentMetal += metalPerTurn;
+        currentOxygen += oxygenPerTurn;
+        currentEnergy += energyPerTurn;
+
+        currentTurn++;
+
+
+    }
+
+    public void SetPlacingBuilding(BuildingType buildingType)
+    {
+        placingBuilding = true;
+        currentSelectedBuilding = buildingType;
+
+    }
+
+    public void OnCreatedNewBuilding(Building building)
+    {
+        if (building.doesProduceResource)
+        {
+            switch(building.productionResource)
+            {
+                case ResourceType.Food:
+                    foodPerTurn += building.productionResourcePerTurn;
+                    break;
+
+                case ResourceType.Metal:
+                    metalPerTurn += building.productionResourcePerTurn;
+                    break;
+
+                case ResourceType.Oxygen:
+                    oxygenPerTurn += building.productionResourcePerTurn;
+                    break;
+
+                case ResourceType.Energy:
+                    energyPerTurn += building.productionResourcePerTurn;
+                    break;
+
+            }
+        }
+
+        if (building.hasMaintenanceCost)
+        {
+            switch (building.maintenanceResource)
+            {
+                case ResourceType.Food:
+                    foodPerTurn += building.maintenanceResourcePerTurn;
+                    break;
+
+                case ResourceType.Metal:
+                    metalPerTurn += building.maintenanceResourcePerTurn;
+                    break;
+
+                case ResourceType.Oxygen:
+                    oxygenPerTurn += building.maintenanceResourcePerTurn;
+                    break;
+
+                case ResourceType.Energy:
+                    energyPerTurn += building.maintenanceResourcePerTurn;
+                    break;
+
+            }
+        }
+
+        placingBuilding = false;
+
     }
 }
